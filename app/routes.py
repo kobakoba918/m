4c937f4075aa5d27f307410209b792d1d26c7679
kobakoba918 <blackpepper8190@gmai.com>
2023-09-20 09:04:51 +0900

    Added about page and updated templates

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,10 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 def index():
-    return "Hello, World"
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
